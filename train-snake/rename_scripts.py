438258f443bd39f27dae03c56e7e2cd14c4335f8
Logan Rowe <rowe1227@gmail.com>
2020-03-03 14:39:44 -0600

    snake resets on injury, high score tracked, scoreboard nad icon added

---
+++ train-snake/rename_scripts.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 14:38:52 2020
+
+@author: Logan Rowe
+"""
+
---
+++ train-snake/run_snake.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 14:34:58 2020
+
+@author: Logan Rowe
+"""
+
+import snake_game
+
